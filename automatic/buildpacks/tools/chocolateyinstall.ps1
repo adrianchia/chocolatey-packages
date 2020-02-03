57f05656c657b86fb170d99496a491cf24da6238
Adrian Chia <achia@adrianchia.com>
2020-02-03 17:05:53 -0600

    Add cloud native buildpacks

---
+++ automatic/buildpacks/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/buildpacks/pack/releases/download/v0.8.1/pack-v0.8.1-windows.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = '7521815af2220fda3a22836e70de89ce795d60decadc87a2452ae48efe9c117f'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/buildpacks/update.ps1
@@ -0,0 +1,28 @@
+﻿import-module au -Force
+
+$releases = 'https://github.com/buildpacks/pack/releases/latest'
+
+function global:au_SearchReplace {
+  @{
+    'tools\chocolateyInstall.ps1' = @{
+      "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+      "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $re  = "pack-((?!(alpha|beta|rc)).)+-windows.zip"
+
+  $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+  $version = ($url -split '-' | select -Last 1 -Skip 1).TrimStart('v')
+  $url32 = 'https://github.com' + $url
+
+  $Latest = @{ URL = $url32; Version = $version }
+  return $Latest
+}
+
+update -ChecksumFor 32
