b6244947eccf29f92590b5cf316127d247d2fae7
Adrian Chia <achia@adrianchia.com>
2020-06-25 11:16:24 -0500

    refactor

--- automatic/istioctl/update.ps1
+++ automatic/istioctl/update.ps1
@@ -13,22 +13,6 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-  <#$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $re  = "istioctl-((?!(alpha|beta|rc)).)+-win.zip"
-
-  $url = $download_page.links | ? href -match $re | select -First 2 -expand href
-
-  $version = $url[0] -split '-' | select -Last 1 -Skip 1
-  $url32 = 'https://github.com' + $url[0]
-
-  $checksum_path = "$($pwd)\.$($url[1] -split  '/' | select -Last 1)"
-  $checksum_url = 'https://github.com' + $url[1]
-  $wc = New-Object net.webclient
-  $wc.Downloadfile($checksum_url, $checksum_path)
-  $checksum = (Get-Content $checksum_path -First 1) -split ' ' | select -First 1
-
-  $Latest = @{ URL = $url32; Checksum32 = $checksum; Version = $version }
-  return $Latest#>
   $Latest1_6 = getLatestByVersionPrefix -Version "1.6"
   $Latest1_5 = getLatestByVersionPrefix -Version "1.5"
   $Latest1_4 = getLatestByVersionPrefix -Version "1.4"
