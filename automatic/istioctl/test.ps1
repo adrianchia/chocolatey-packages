07584f550a6d9faae86782ac3a828e5ae2cb2724
Adrian Chia <achia@adrianchia.com>
2023-02-01 18:08:45 -0600

    Test

--- automatic/istioctl/test.ps1
+++ automatic/istioctl/test.ps1
@@ -3,24 +3,22 @@ import-module au -Force
 $releases = 'https://github.com/istio/istio/releases'
 
 function getLatestByVersionPrefix {
-    param (
+  param (
       [string]$Version
     )
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $release_tag_link = $download_page.links | ? href -match $Version | ? href -match 'releases/tag' | select -First 1 -expand href
     $re  = "istioctl-((?!(alpha|beta|rc)).)+-win.zip"
-    $url = $download_page.links | ? href -match $re | ? href -match $Version | select -First 2 -expand href
-    $version = $url[0] -split '-' | select -Last 1 -Skip 1
-    $url32 = 'https://github.com' + $url[0]
+    $version = $release_tag_link -split '/' | select -Last 1
+    # Download release info from api instead of defaut release page
+    $api_link = 'https://api.github.com/repos/istio/istio/releases/tags/' + $version
+    $url = Invoke-WebRequest -Uri $api_link `
+    | ConvertFrom-Json `
+    | Select-Object -ExpandProperty assets `
+    | Where-Object {$_.name -match $re } `
+    | Select-Object -ExpandProperty browser_download_url
+    Write-Output $url
 
-    $checksum_path = "$($pwd)\.$($url[1] -split  '/' | select -Last 1)"
-    $checksum_url = 'https://github.com' + $url[1]
-    $wc = New-Object net.webclient
-    $wc.Downloadfile($checksum_url, $checksum_path)
-    $checksum = (Get-Content $checksum_path -First 1) -split ' ' | select -First 1
+}
 
-    $Latest = @{ URL = $url32; Checksum32 = $checksum; Version = $version }
-    return $Latest
-  }
-
-$1_7 = getLatestByVersionPrefix -Version "1.7"
-Write-Output $1_7
+getLatestByVersionPrefix -Version "1.16"
--- automatic/istioctl/update.ps1
+++ automatic/istioctl/update.ps1
@@ -27,13 +27,24 @@ function getLatestByVersionPrefix {
     param (
       [string]$Version
     )
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $release_tag_link = $download_page.links | ? href -match $Version | ? href -match 'releases/tag' | select -First 1 -expand href
     $re  = "istioctl-((?!(alpha|beta|rc)).)+-win.zip"
-    $ReleaseVersion= "releases/" + $Version
-    $url = $download_page.links | ? href -match $Version | ? href -match $ReleaseVersion | select -First 1 -expand href
-    $artifact_page = Invoke-WebRequest -Uri $url
-    $url = $artifact_page.links | ? href -match $re | select -First 2 -expand href
-    $version = $url[0] -split '-' | select -Last 1 -Skip 1
+    #$ReleaseVersion= "releases/" + $Version
+    #$url = $download_page.links | ? href -match $Version | ? href -match $ReleaseVersion | select -First 1 -expand href
+    #$artifact_page = Invoke-WebRequest -Uri $url
+    #$url = $artifact_page.links | ? href -match $re | select -First 2 -expand href
+    #$version = $url[0] -split '-' | select -Last 1 -Skip 1
+    #$url32 = $url[0]
+    $version = $release_tag_link -split '/' | select -Last 1
+    # Download release info from api instead of defaut release page
+    $api_link = 'https://api.github.com/repos/istio/istio/releases/tags/' + $version
+    $url = Invoke-WebRequest -Uri $api_link `
+    | ConvertFrom-Json `
+    | Select-Object -ExpandProperty assets `
+    | Where-Object {$_.name -match $re } `
+    | Select-Object -ExpandProperty browser_download_url
+    Write-Output $url
     $url32 = $url[0]
 
     $checksum_path = "$($pwd)\.$($url[1] -split  '/' | select -Last 1)"
