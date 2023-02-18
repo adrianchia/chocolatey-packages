ddb3a2e69018a24aeddd36e39e8ffda88e55d651
Adrian Chia <achia@adrianchia.com>
2023-02-17 22:14:27 -0600

    Add istio 1.17

--- automatic/istioctl/update.ps1
+++ automatic/istioctl/update.ps1
@@ -12,11 +12,13 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
+  $Latest1_17 = getLatestByVersionPrefix -Version "1.17"
   $Latest1_16 = getLatestByVersionPrefix -Version "1.16"
   $Latest1_15 = getLatestByVersionPrefix -Version "1.15"
 
   @{
     Streams = [ordered] @{
+      '1.17' = @{Version = $Latest1_17.Version; URL = $Latest1_17.URL; Checksum32 = $Latest1_17.Checksum32}
       '1.16' = @{Version = $Latest1_16.Version; URL = $Latest1_16.URL; Checksum32 = $Latest1_16.Checksum32}
       '1.15' = @{Version = $Latest1_15.Version; URL = $Latest1_15.URL; Checksum32 = $Latest1_15.Checksum32}
     }
