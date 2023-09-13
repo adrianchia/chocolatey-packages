e3aba7337383a2fd5618a7b0f3db2d064a0d4f06
Adrian Chia <achia@adrianchia.com>
2023-09-13 02:45:51 +0000

    AU: 1 updated - istioctl [skip ci]

--- automatic/istioctl/tools/chocolateyinstall.ps1
+++ automatic/istioctl/tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/istio/istio/releases/download/1.17.3/istioctl-1.17.3-win.zip'
+$url        = 'https://github.com/istio/istio/releases/download/1.18.3/istioctl-1.18.3-win.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = 'f7a2d69eb0d771098dc8a828314f9aef9b3f014c949cea4b707c02cdd67eb88d'
+  checksum      = '27a2ff0dac7f9d77de05d2a3520306c6d307e71c8a635bd44ea7d41a61c393d2'
   checksumType  = 'sha256'
 }
 
